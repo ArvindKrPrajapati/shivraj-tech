1f186fd624b172be44522f70bb7a27e7253188f2
ArvindKrPrajapati <arvindsoft37@gmail.com>
2022-10-27 19:57:19 +0530

    initial commit

--- data.js
+++ data.js
@@ -11,6 +11,16 @@ const data = [
     },
     {
         id: 2,
+        image: "https://miro.medium.com/max/828/1*cGDDA2mfYkjiIhGaN8gDoA.png",
+        title: "Educate Me | Video Source To Learn Different Technologies",
+        description: "This website will collect best tutorial videos present in youtube on different topics to overcome the effort to search best videos for you study on a topic .",
+        linkText: "Visit",
+        link: "https://greenhat-sr.netlify.app/home",
+        techUsed: "Angular , Angular Material",
+        price: "5,000"
+    },
+    {
+        id: 3,
         image: "./assets/mverse.png",
         title: "MVerse | Watch Any Movie , Tv-Series Free",
         description: "Watch Any Movie Tv-Series Free Availabe Only On MVerse App",
@@ -21,13 +31,24 @@ const data = [
 
     },
     {
-        id: 3,
+        id: 4,
         image: "./assets/shorts.png",
         title: "Shorts | Let's Make it",
         description: "Create Your Shorts Video And Share With The World ,Gain Folloewrs And Depending On Your Reach And Public Engagemet , Get Varified",
         linkText: "Download Apk",
         link: "https://drive.google.com/file/d/1XCVpXUty-iD7krZ9_zbdztMnj_QT_JPN/view?usp=drivesdk",
         techUsed: "React Native , NodeJs, MongoDb",
+        price: "20,000"
+
+    },
+    {
+        id: 5,
+        image: "./assets/chatverse.png",
+        title: "Chatverse",
+        description: "Real Time chat application , where yo can chat with anyone in realtime",
+        linkText: "Download Apk",
+        link: "https://drive.google.com/file/d/1Nuo2BjMKN-Q1CPG3nquQClj25prNnl9U/view?usp=drivesdk",
+        techUsed: "React Native ,Firebase",
         price: "20,000"
 
     },
