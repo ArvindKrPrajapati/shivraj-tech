127ad325662cb01db6896cfd5b8815f88339e522
ArvindKrPrajapati <arvindsoft37@gmail.com>
2022-10-27 19:13:08 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        id: 1,
+        image: "./assets/equals.png",
+        title: "Equals | Social Media",
+        description: "This Social Media App Is Like Any Other Social Media You Can Share Pictures , Thoughts, Videos ,Link With Worlds .",
+        linkText: "Visit",
+        link: "https://equals-angular.herokuapp.com",
+        techUsed: "Angular , NodeJs ,MongoDb",
+        price: "10,000"
+    },
+    {
+        id: 2,
+        image: "./assets/mverse.png",
+        title: "MVerse | Watch Any Movie , Tv-Series Free",
+        description: "Watch Any Movie Tv-Series Free Availabe Only On MVerse App",
+        linkText: "Download Apk",
+        link: "https://drive.google.com/file/d/1pTg-CM3qdOpilLf5Et7_oq5x6KzIGx7A/view?usp=sharing",
+        techUsed: "React Native and TMDB API",
+        price: "20,000"
+
+    },
+    {
+        id: 3,
+        image: "./assets/shorts.png",
+        title: "Shorts | Let's Make it",
+        description: "Create Your Shorts Video And Share With The World ,Gain Folloewrs And Depending On Your Reach And Public Engagemet , Get Varified",
+        linkText: "Download Apk",
+        link: "https://drive.google.com/file/d/1XCVpXUty-iD7krZ9_zbdztMnj_QT_JPN/view?usp=drivesdk",
+        techUsed: "React Native , NodeJs, MongoDb",
+        price: "20,000"
+
+    },
+]
